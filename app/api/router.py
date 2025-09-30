abbe2090d4400ef2badbb7e545a698fda901281a
EymireWorld <taras.gordon2@gmail.com>
2025-09-30 15:45:24 +0300

    Init characters

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .characters.router import router as characters_router
 from .main.router import router as main_router
 from .materials.router import router as materials_router
 from .weapons.router import router as weapons_router
@@ -10,6 +11,11 @@ router = APIRouter()
 router.include_router(
     main_router,
     tags=['Main'],
+)
+router.include_router(
+    characters_router,
+    prefix='/characters',
+    tags=['Characters'],
 )
 router.include_router(
     weapons_router,
